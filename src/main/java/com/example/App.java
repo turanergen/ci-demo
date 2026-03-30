534c86445000ab83e37e9d12662d3c904199fec6
Turan Ergen <turanergen@gmail.com>
2026-03-30 10:49:41 -0700

    Breaking Commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,7 +8,7 @@ public class App {
         System.out.println("divide(5,2) = " + divide(5, 2));
     }
 
-    public static int add(int a, int b) { return a + b; }
+    public static int add(int a, int b) { return a + b + 1; }
     public static int subtract(int a, int b) { return a - b; }
     public static int multiply(int a, int b) { return a * b; }
     public static double divide(int a, int b) {
